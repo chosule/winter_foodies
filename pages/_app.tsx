c3de998e4a86373b1c3869bc928c7fc130942688
chosule <89799325+chosule@users.noreply.github.com>
2023-09-06 01:06:37 +0900

    feat: next 초기 셋팅

---
+++ pages/_app.tsx
@@ -0,0 +1,13 @@
+import type { AppProps } from "next/app";
+
+/**
+ *
+ * @description
+ * 서버로 요청이 들어왔을때 가장 먼저 실행 되는 컴포넌트
+ */
+
+const MyApp = () => {
+  return;
+};
+
+export default MyApp;
---
+++ types/commons.tsx
@@ -0,0 +1,4 @@
+import { NextPage } from "next";
+import React from "react";
+
+export type NextPageWithLayout
