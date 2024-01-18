8834c30118c76413427d92c283ad00a5ddd8732c
chosule <89799325+chosule@users.noreply.github.com>
2024-01-19 00:10:54 +0900

    🛠 :react query 직렬화 issue....

--- api/getpost.ts
+++ api/getpost.ts
@@ -1,10 +1,11 @@
-
-
+import getNearbyData, { getHeartStore } from "@/libs/productApi";
+import { deleteUndefined } from "@/hooks/useDeleteUndefined";
 
 export async function getPosts() {
-  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
-  const posts = (await response.json());
-  return posts;
+  const response = await getHeartStore();
+  // const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  // const posts = await response;
+  return response;
 }
 
 export default getPosts;
--- pages/post/index.tsx
+++ pages/post/index.tsx
@@ -1,39 +1,33 @@
 import { useQuery } from "@tanstack/react-query";
 import styled from "@emotion/styled";
-import getPosts from "../../api/getpost"
+import getPosts from "../../api/getpost";
+import { deleteUndefined } from "@/hooks/useDeleteUndefined";
 
-
+const removeUndefinedForNextJsSerializing = <T,>(props: T): T =>
+  Object.fromEntries(
+    Object.entries(props).filter(([, value]) => value !== undefined)
+  ) as T;
 
 export async function getStaticProps() {
-    const posts = await getPosts();
-    return { props: { posts } }
-  }
+  const posts = await getPosts();
+  const data = JSON.stringify(posts);
+  return {
+    props: removeUndefinedForNextJsSerializing({
+      data,
+    }),
+  };
+}
 
 const PostsPage = (props) => {
-    const { data ,isLoading} = useQuery({
-        queryKey: ['posts'],
-        queryFn: getPosts,
-        initialData: props.posts,
-      })
- 
+  const { data, isLoading } = useQuery({
+    queryKey: ["posts"],
+    queryFn: getPosts,
+    initialData: props.posts,
+  });
 
   if (isLoading || !data) return <div>is Loading.. </div>;
 
-  return (
-    <StyledFlex>
-      <div>
-        <h2 className="text-lg text-center font-bold mb-6">
-          React Query SSR 적용
-        </h2>
-        <ul>
-          {data?.map((post) => (
-            <li key={post.id}>{post.title}</li>
-          ))}
-        </ul>
-      </div>
-     
-    </StyledFlex>
-  );
+  return <div>{JSON.stringify(data)}</div>;
 };
 
 const StyledFlex = styled.div`
