43a7b95dcc06872be39409a037fab2c35a5d1ade
chosule <89799325+chosule@users.noreply.github.com>
2024-06-19 21:22:46 +0900

    🔥 Delete: 리팩토링으로 인한 코드 삭제

--- src/types/commons.tsx
+++
@@ -1,8 +0,0 @@
-import { NextPage } from "next";
-import React, { ReactElement, ReactNode } from "react";
-
-export type NextPageWithLayout<P = {}> = NextPage<P> & {
-  getLayout?: (page: Children) => Children;
-};
-
-export type Children = React.ReactNode;
