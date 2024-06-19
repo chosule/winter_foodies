bf946c66147c13f3b935d5ad8337cb574241eab9
chosule <89799325+chosule@users.noreply.github.com>
2024-06-19 21:27:27 +0900

    ♻️ Refactor: headerLayout 리팩토링 및 매장 찜하기, 매장 찜하기 삭제 적용

--- src/app/_component/HeaderLayout.tsx
+++ src/app/_component/HeaderLayout.tsx
@@ -1,16 +1,51 @@
+"use client";
 import { useRouter } from "next/navigation";
 import StarRating from "./StarRating";
 import { FaArrowLeft } from "react-icons/fa";
+import heartIcon from "../../../public/img/heart.svg";
+import heartFillIcon from "../../../public/img/fillHeart.svg";
+import Image from "next/image";
+import { useSession } from "next-auth/react";
+import getLike from "../(afterLogin)/_lib/getLike";
+import { MouseEventHandler } from "react";
+import like from "../(afterLogin)/_lib/like";
+import unLike from "../(afterLogin)/_lib/unLike";
+import { mutate } from "swr";
 
 type Props = {
   headerTitle: string;
   storeRating?: string;
+  heart?: boolean;
+  storeName?: string;
 };
 
-const HeaderLayout = ({ headerTitle, storeRating }: Props) => {
+const HeaderLayout = ({
+  headerTitle,
+  storeRating,
+  heart,
+  storeName,
+}: Props) => {
   const router = useRouter();
+  const { data } = useSession();
+  const { data: getLikes } = getLike();
+
+  const { trigger: likeTrigger } = like();
+  const { trigger: unLikeTrigger } = unLike();
+
+  const liked = !!getLikes?.data?.find((item) => item.storeName === storeName);
+
+  const onLike: MouseEventHandler<HTMLButtonElement> = async (e) => {
+    e.preventDefault();
+    if (liked) {
+      await unLikeTrigger({ userId: data?.user?.id, storeName });
+    } else {
+      await likeTrigger({ userId: data?.user?.id, storeName });
+    }
+    mutate([`${process.env.NEXT_PUBLIC_BASE_URL}/like`, data?.user?.id]);
+  };
+
   return (
-    <div className="w-full grid grid-cols-3 items-center h-[77px]">
+    <div className="w-full grid grid-cols-3 items-center h-[77px] relative">
       <button
         onClick={() => {
           router.back();
@@ -18,9 +53,23 @@ const HeaderLayout = ({ headerTitle, storeRating }: Props) => {
       >
         <FaArrowLeft />
       </button>
-      <div className="flex items-center">
+      <div className="flex items-center justify-center">
         {headerTitle && <p className="text-[20px] font-bold">{headerTitle}</p>}
         {storeRating && <StarRating storeRating={storeRating} />}
+        {heart && (
+          <button className="absolute right-0" onClick={onLike}>
+            {liked ? (
+              <Image
+                src={heartFillIcon}
+                alt="하트아이콘"
+                width={20}
+                height={20}
+              />
+            ) : (
+              <Image src={heartIcon} alt="하트아이콘" width={20} height={20} />
+            )}
+          </button>
+        )}
       </div>
     </div>
   );
