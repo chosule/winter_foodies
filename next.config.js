4d42138404c3206cce9ebbefc7375f9388e05db2
chosule <89799325+chosule@users.noreply.github.com>
2024-06-26 15:49:34 +0900

    Resolve merge conflicts in next.config.js, package-lock.json, and package.json

--- next.config.js
+++ next.config.js
@@ -1,6 +1,27 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  /* config options here */
-};
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
+const nextConfig = withBundleAnalyzer({
+  experimental: {
+    appDir: true,
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
+  images: {
+    domains: [
+      "encrypted-tbn0.gstatic.com",
+      "mblogthumb-phinf.pstatic.net",
+      "media.istockphoto.com",
+      "cdn.pixabay.com",
+      "via.placeholder.com",
+    ],
+  },
+});
 
 module.exports = nextConfig;
