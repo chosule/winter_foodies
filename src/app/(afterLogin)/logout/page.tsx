83c38b132f41432be197ef2e3cc30e847a5a0128
chosule <89799325+chosule@users.noreply.github.com>
2024-06-26 20:26:16 +0900

    ♻️ Refactor: 로그아웃 next-auth signOut으로 리팩토링

--- src/app/(afterLogin)/logout/page.tsx
+++ src/app/(afterLogin)/logout/page.tsx
@@ -1,19 +1,11 @@
 "use client";
-import DefaultLayout from "@/src/components/layouts/Default";
-import useContextModal from "@/src/context/hooks/useContextModal";
-import { useEffect } from "react";
+import { auth } from "@/auth";
+import { signOut } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
-export default function LogoutPage() {
-  const modal = useContextModal();
-
-  const openLogoutModal = () => {
-    modal.logoutModal({
-      message: "로그아웃 하시겠습니까?",
-    });
-  };
-
-  useEffect(() => {
-    openLogoutModal();
-  }, []);
-  return <></>;
+export default function page() {
+  const router = useRouter();
+  signOut({ redirect: false }).then(() => {
+    router.replace("/");
+  });
 }
