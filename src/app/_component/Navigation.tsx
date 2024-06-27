69d142be97b943a01dc7d60f4e9be8a0e3ddda43
chosule <89799325+chosule@users.noreply.github.com>
2024-06-27 22:56:38 +0900

    ♻️ Refactor: navigation 리팩토링

--- src/app/_component/Navigation.tsx
+++ src/app/_component/Navigation.tsx
@@ -1,112 +1,87 @@
-// import {
-//   HomeIcon,
-//   HomeIconActive,
-//   MypageIcon,
-//   MypageIconActive,
-//   AroundmeIcon,
-//   AroundmeIconActive,
-//   CartIcon,
-//   CartIconActive,
-//   LoginIcon,
-//   LoginIconActive,
-// } from "@/src/components/Icon/BottomNavigation";
-// import Link from "next/link";
-// import { NaviUI } from "./style";
-// import { usePathname } from "next/navigation";
-// import CartStatus from "@/src/app/(afterLogin)/_component/CartStatus";
-// import { userState } from "@/src/recoil/atom";
-// import { useRecoilValue } from "recoil";
-// import { useEffect, useState } from "react";
-// import useUserAuth from "@/src/hooks/auth/useUserAuth";
+"use client";
+import {
+  HomeIcon,
+  HomeIconActive,
+  MypageIcon,
+  MypageIconActive,
+  AroundmeIcon,
+  AroundmeIconActive,
+  CartIcon,
+  CartIconActive,
+  LoginIcon,
+  LoginIconActive,
+} from "../../../public/img/navigation_icon";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useSession } from "next-auth/react";
 
-// const routes = [
-//   {
-//     text: "홈",
-//     img: {
-//       default: <HomeIcon />,
-//       active: <HomeIconActive />,
-//     },
-//     path: "/",
-//   },
-//   {
-//     text: "내주변",
-//     img: {
-//       default: <AroundmeIcon />,
-//       active: <AroundmeIconActive />,
-//     },
-//     path: "/aroundme",
-//   },
-//   {
-//     text: "장바구니",
-//     img: {
-//       default: <CartIcon />,
-//       active: <CartIconActive />,
-//     },
-//     path: "/cart",
-//     cartStatus: true,
-//   },
-//   {
-//     text: "마이페이지",
-//     img: {
-//       default: <MypageIcon />,
-//       active: <MypageIconActive />,
-//     },
-//     path: "/mypage",
-//   },
-//   {
-//     text: "로그인",
-//     img: {
-//       default: <LoginIcon />,
-//       active: <LoginIconActive />,
-//     },
-//     path: "/login",
-//     tokenState: false,
-//   },
-// ];
+export const BottomNavigation = () => {
+  const pathname = usePathname();
+  const { data, status } = useSession();
+  const routes = [
+    {
+      text: "홈",
+      img: {
+        default: <HomeIcon />,
+        active: <HomeIconActive />,
+      },
+      link: "/",
+    },
+    {
+      text: "내주변",
+      img: {
+        default: <AroundmeIcon />,
+        active: <AroundmeIconActive />,
+      },
+      link: "/aroundme",
+    },
+    {
+      text: "장바구니",
+      img: {
+        default: <CartIcon />,
+        active: <CartIconActive />,
+      },
+      link: "/cart",
+      cartStatus: true,
+    },
+    {
+      text: "마이페이지",
+      img: {
+        default: <MypageIcon />,
+        active: <MypageIconActive />,
+      },
+      link: "/mypage",
+    },
+    {
+      text: `${data?.user ? "로그아웃" : "로그인"}`,
+      img: {
+        default: <LoginIcon />,
+        active: <LoginIconActive />,
+      },
+      link: `${data?.user ? "/logout" : "/login"}`,
+    },
+  ];
+  return (
+    <nav className="nav">
+      {routes.map(({ link, text, img }) => {
+        const isActive = pathname === link;
+        return (
+          <Link key={text} href={link}>
+            <div className="flex flex-col relative items-center gap-[5px] w-full">
+              {isActive ? img.active : img.default}
+              <p
+                className={`text-[12px] ${
+                  isActive ? "text-black" : "text-[#ddd]"
+                }`}
+              >
+                {text}
+              </p>
+            </div>
+          </Link>
+        );
+      })}
+    </nav>
+  );
+};
 
-// const BottomNavigation = () => {
-//   const token = useRecoilValue(userState);
-//   const pathname = usePathname();
-
-//   const [loginText, setLoginText] = useState(routes[4]);
-
-//   useEffect(() => {
-//     const token = localStorage.getItem("accessToken");
-//     if (token) {
-//       setLoginText((prev) => ({ ...prev, text: "로그아웃", path: "/logout" }));
-//     } else {
-//       setLoginText((prev) => ({ ...prev, text: "로그인", path: "/login" }));
-//     }
-//   }, [token]);
-
-//   return (
-//     <>
-//       <div className="w-full h-[95px]">
-//         <nav className="nav">
-//           {routes.map(({ path, text, img, cartStatus }) => {
-//             const isActive = pathname === path;
-//             const buttonText = text === "로그인" ? loginText.text : text;
-//             const paths = text === "로그인" ? loginText.path : path;
-//             return (
-//               <Link key={text} href={paths} passHref>
-//                 <div className="flex flex-col relative items-center gap-[5px] w-full">
-//                   {isActive ? img.active : img.default}
-//                   <p
-//                     className={`text-[12px] ${
-//                       isActive ? "text-black" : "text-[#ddd]"
-//                     }`}
-//                   >
-//                     {buttonText}
-//                   </p>
-//                   {cartStatus && <CartStatus />}
-//                 </div>
-//               </Link>
-//             );
-//           })}
-//         </nav>
-//       </div>
-//     </>
-//   );
-// };
-
-// export default BottomNavigation;
+export default BottomNavigation;
