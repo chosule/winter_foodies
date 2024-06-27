c4aada42017c9b8096407c900ab456e0c6932da7
chosule <89799325+chosule@users.noreply.github.com>
2024-06-27 22:57:26 +0900

    ✨ Add: 메뉴 이미지 추가 및 경로 추가

--- src/app/(beforeLogin)/menu/[menuId]/_component/MenuPost.tsx
+++ src/app/(beforeLogin)/menu/[menuId]/_component/MenuPost.tsx
@@ -1,20 +1,28 @@
 import Link from "next/link";
 import { GetMenuData } from "../_model/menu";
 import Rating from "@/app/_component/Rating";
+import getStoreMenu from "@/app/(beforeLogin)/store/[menuId]/[menu]/[storeName]/_lib/getStoreMenu";
+import Image from "next/image";
 
 type Props = {
   menu: GetMenuData;
+  menuId: string;
 };
-export default function MenuPost({ menu }: Props) {
+
+export default function MenuPost({ menu, menuId }: Props) {
   return (
     <div className="flex flex-col gap-3">
       <Link
-        href="#"
+        href={`/store/${menuId}/menu/${menu.store}`}
         className="menu-grid w-full border rounded-md items-center  hover:border-color-orange grid-cols-3 bg-color-white"
       >
-        <div className="bg-color-orange h-full w-[65px] rounded-md">
-          {/* <Image src="" alt="사진" /> */}
-        </div>
+        <Image
+          className="rounded-md"
+          src={menu.image}
+          alt="사진"
+          width={70}
+          height={75}
+        />
         <div className="py-3 flex flex-col gap-1">
           <h1 className="text-[14px] font-medium">{menu.store}</h1>
           <div className="flex gap-2">
