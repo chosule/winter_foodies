58cda0779497b6306deb4853b70eecf230e941d3
chosule <89799325+chosule@users.noreply.github.com>
2024-06-27 22:57:55 +0900

    💄 Style: 모달 css 추가

--- src/app/_component/Modal.tsx
+++ src/app/_component/Modal.tsx
@@ -4,18 +4,19 @@ import { TiDelete } from "react-icons/ti";
 
 type Props = {
   children: React.ReactNode;
+  className?: string;
 };
 
-export default function Modal({ children }: Props) {
+export default function Modal({ children, className }: Props) {
   const router = useRouter();
   const onClickClose = () => {
     router.back();
   };
   return (
     <div className="w-full h-screen flex justify-center absolute bg-color-modalBg top-0 left-0 right-0 bottom-0 modal-bg items-center">
-      <div className="bg-color-white rounded-md p-4">
+      <div className={`bg-color-white rounded-md relative ${className}`}>
         <TiDelete
-          className="text-[30px] ml-auto cursor-pointer"
+          className="text-[30px] ml-auto cursor-pointer absolute top-0 right-0"
           onClick={onClickClose}
         />
         {children}
